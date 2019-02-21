db543b3fe26c1a128093c0364eef5e848c88d8e1
amritrau <amritrau@gmail.com>
2019-02-20 22:20:40 -0500

    Started GameContext class

---
+++ shadow-hunters/game_context.py
@@ -0,0 +1,52 @@
+import random
+
+from die import Die
+from zone import Zone
+# game_context.py
+# Implements a GameContext.
+
+class GameContext:
+    def __init__(self, players, characters, black_cards, white_cards, green_cards, areas, modifiers = dict()):
+        self.players = players
+        self.black_cards = black_cards
+        self.white_cards = white_cards
+        self.green_cards = green_cards
+        self.modifiers = modifiers
+
+        # Instantiate dice
+        self.die4 = Die(4)
+        self.die6 = Die(6)
+
+        # Randomly shuffle areas across zones
+        random.shuffle(areas)
+        self.zones = [zone.Zone([areas.pop(), areas.pop()] for i in range(3)]
+
+        # Randomly assign characters
+        random.shuffle(characters)
+        for player in self.players:
+            player.character = characters.pop()
+
+
+    def checkWinConditions(self):
+        return [p for p in players if p.win_cond()]
+
+    def play(self):
+        """
+        Game loop
+        """
+
+        while True:
+            # TODO Game action here
+            raise NotImplementedError
+
+
+            winners = self.checkWinConditions()
+            if winners:
+                return winners
+
+    def dump(self):
+        """
+        Return the full GameContext in JSON for debugging
+        """
+        raise NotImplementedError
+        return ""
