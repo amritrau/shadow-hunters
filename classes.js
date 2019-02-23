3e0d85f612248d9c2cd12534fad1fd59117807a7
Joanna Wu <joannawu@Joannas-MacBook-Air.local>
2019-02-23 14:17:47 -0500

    added health bar

--- classes.js
+++ classes.js
@@ -25,12 +25,53 @@ var GameBoard = new Phaser.Class ({
             'assets/dude.png',
             { frameWidth: 32, frameHeight: 48 }
         );
+        this.load.image('0', 'assets/zero.png');
+        this.load.image('1', 'assets/one.png');
+        this.load.image('2', 'assets/two.png');
+        this.load.image('3', 'assets/three.png');
+        this.load.image('4', 'assets/four.png');
+        this.load.image('5', 'assets/five.png');
+        this.load.image('6', 'assets/six.png');
+        this.load.image('7', 'assets/seven.png');
+        this.load.image('8', 'assets/eight.png');
+        this.load.image('9', 'assets/nine.png');
+        this.load.image('10', 'assets/ten.png');
+        this.load.image('11', 'assets/eleven.png');
+        this.load.image('12', 'assets/twelve.png');
+        this.load.image('13', 'assets/thirteen.png');
+        this.load.image('14', 'assets/fourteen.png');
+        this.load.image('text', 'assets/text.png');
+
+
+
     },
 
     create: function () {
         this.add.image(400, 300, 'sky');
+        this.add.image(700,20, '14');
+        this.add.image(700,60, '13');
+        this.add.image(700,100, '12');
+        this.add.image(700,140, '11');
+        this.add.image(700,180, '10');
+        this.add.image(700,220, '9');
+        this.add.image(700,260, '8');
+        this.add.image(700,300, '7');
+        this.add.image(700,340, '6');
+        this.add.image(700,380, '5');
+        this.add.image(700,420, '4');
+        this.add.image(700,460, '3');
+        this.add.image(700,500, '2');
+        this.add.image(700,540, '1');
+        this.add.image(700,580, '0');
+
+
         //this.player = this.add.sprite(100, 450, 'dude');
         //this.player.name = "player1";
+
+        this.makeBox();
+        this.block = this.add.image(this.player.x +20, this.player.y, "text");
+        this.block.setVisible(false);
+        this.player.on('clicked', this.clickHandler, this.block);
 
         this.makePlayer();
         this.block = this.add.image(this.player.x, this.player.y -60, "customTip");
@@ -52,6 +93,13 @@ var GameBoard = new Phaser.Class ({
         // });
 
         //this.time.delayedCall(0, this.startInputEvents, [], this);
+    },
+
+    makeBox: function() {
+        var box  = this.add.box(800, 20, 'text');
+        box.name = 'text';
+        box.setInteractive();
+        this.player = box;
     },
 
     makePlayer: function () {
