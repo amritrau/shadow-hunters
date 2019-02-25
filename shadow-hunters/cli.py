fb4b7129f5061e9e3da2515ff08349ad9e5772bb
Amrit Rau <amritrau@users.noreply.github.com>
2019-02-25 12:18:09 -0500

    Add GameContext to app.py (#13)
    * Add GameContext to app.py
    
    * Update initalization, socket test for @giareyes

--- shadow-hunters/cli.py
+++ shadow-hunters/cli.py
@@ -1,8 +1,10 @@
-import card, deck
+import card, deck, character, area
 
 # cli.py
 # Provides a CLI to the game.
 # For now -- run with python -i cli.py
+
+# TODO Hermit cards, Characters, and Areas
 
 # Initialize cards and decks
 WHITE_CARDS = [
@@ -13,7 +15,7 @@ WHITE_CARDS = [
         holder = None,
         is_equip = True,
         force_use = False,
-        use = lambda: 0  # placeholder
+        use = lambda: 0  # TODO
     ),
     card.Card(
         title = "Flare of Judgement",
@@ -22,7 +24,7 @@ WHITE_CARDS = [
         holder = None,
         is_equip = False,
         force_use = True,
-        use = lambda: 0 # placeholder
+        use = lambda: 0 # TODO
     ),
     card.Card(
         title = "First Aid",
@@ -31,7 +33,7 @@ WHITE_CARDS = [
         holder = None,
         is_equip = False,
         force_use = True,
-        use = lambda: 0 # placeholder
+        use = lambda: 0 # TODO
     ),
     card.Card(
         title = "Holy Water of Healing",
@@ -40,7 +42,7 @@ WHITE_CARDS = [
         holder = None,
         is_equip = False,
         force_use = True,
-        use = lambda: 0 # placeholder
+        use = lambda: 0 # TODO
     ),
     card.Card(
         title = "Holy Water of Healing",
@@ -49,7 +51,7 @@ WHITE_CARDS = [
         holder = None,
         is_equip = False,
         force_use = True,
-        use = lambda: 0 # placeholder
+        use = lambda: 0 # TODO
     )
 ]
 
@@ -61,7 +63,7 @@ BLACK_CARDS = [
         holder = None,
         is_equip = True,
         force_use = False,
-        use = lambda: 0  # placeholder
+        use = lambda: 0  # TODO
     ),
     card.Card(
         title = "Chainsaw",
@@ -70,7 +72,7 @@ BLACK_CARDS = [
         holder = None,
         is_equip = True,
         force_use = False,
-        use = lambda: 0  # placeholder
+        use = lambda: 0  # TODO
     ),
     card.Card(
         title = "Rusted Broad Axe",
@@ -79,7 +81,7 @@ BLACK_CARDS = [
         holder = None,
         is_equip = True,
         force_use = False,
-        use = lambda: 0  # placeholder
+        use = lambda: 0  # TODO
     ),
     card.Card(
         title = "Moody Goblin",
@@ -88,7 +90,7 @@ BLACK_CARDS = [
         holder = None,
         is_equip = False,
         force_use = True,
-        use = lambda: 0 # placeholder
+        use = lambda: 0 # TODO
     ),
     card.Card(
         title = "Moody Goblin",
@@ -97,7 +99,7 @@ BLACK_CARDS = [
         holder = None,
         is_equip = False,
         force_use = True,
-        use = lambda: 0 # placeholder
+        use = lambda: 0 # TODO
     ),
     card.Card(
         title = "Bloodthirsty Spider",
@@ -106,7 +108,7 @@ BLACK_CARDS = [
         holder = None,
         is_equip = False,
         force_use = True,
-        use = lambda: 0 # placeholder
+        use = lambda: 0 # TODO
     ),
     card.Card(
         title = "Vampire Bat",
@@ -115,7 +117,7 @@ BLACK_CARDS = [
         holder = None,
         is_equip = False,
         force_use = True,
-        use = lambda: 0 # placeholder
+        use = lambda: 0 # TODO
     ),
     card.Card(
         title = "Vampire Bat",
@@ -124,7 +126,7 @@ BLACK_CARDS = [
         holder = None,
         is_equip = False,
         force_use = True,
-        use = lambda: 0 # placeholder
+        use = lambda: 0 # TODO
     ),
     card.Card(
         title = "Vampire Bat",
@@ -133,9 +135,106 @@ BLACK_CARDS = [
         holder = None,
         is_equip = False,
         force_use = True,
-        use = lambda: 0 # placeholder
+        use = lambda: 0 # TODO
     )
 ]
 
 WHITE_DECK = deck.Deck(cards = WHITE_CARDS)
 BLACK_DECK = deck.Deck(cards = BLACK_CARDS)
+GREEN_DECK = deck.Deck(cards = [])
+
+
+# Initialize characters
+CHARACTERS = [
+    character.Character(
+        name = "Valkyrie",
+        alleg = 0,  # Shadow
+        max_hp = 13,
+        win_cond = lambda: 0,  # TODO
+        win_cond_desc = "All the Hunter characters are dead or 3 Neutral characters are dead",
+        special = lambda: 0,  # TODO
+        resource_id = "valkyrie"
+    ),
+    character.Character(
+        name = "Vampire",
+        alleg = 0,  # Shadow
+        max_hp = 13,
+        win_cond = lambda: 0,  # TODO
+        win_cond_desc = "All the Hunter characters are dead or 3 Neutral characters are dead",
+        special = lambda: 0,  # TODO
+        resource_id = "vampire"
+    ),
+    character.Character(
+        name = "Allie",
+        alleg = 1,  # Neutral
+        max_hp = 8,
+        win_cond = lambda: 0,  # TODO
+        win_cond_desc = "You're not dead when the game is over",
+        special = lambda: 0,  # TODO
+        resource_id = "allie"
+    ),
+    character.Character(
+        name = "George",
+        alleg = 2,  # Hunter
+        max_hp = 14,
+        win_cond = lambda: 0,  # TODO
+        win_cond_desc = "All the Shadow characters are dead",
+        special = lambda: 0,  # TODO
+        resource_id = "george"
+    ),
+    character.Character(
+        name = "Fu-ka",
+        alleg = 2,  # Hunter
+        max_hp = 12,
+        win_cond = lambda: 0,  # TODO
+        win_cond_desc = "All the Shadow characters are dead",
+        special = lambda: 0,  # TODO
+        resource_id = "fu-ka"
+    )
+]
+
+# Initialize areas
+AREAS = [
+    area.Area(
+        name = "Hermit's Cabin",
+        desc = "You may draw a Hermit Card.",
+        domain = [2, 3],
+        action = lambda gc, player: player.drawCard(gc.green_cards),
+        resource_id = "hermits-cabin"
+    ),
+    area.Area(
+        name = "Underworld Gate",
+        desc = "You may draw a card from the stack of your choice.",
+        domain = [4, 5],
+        action = lambda gc, player: 0,  # TODO
+        resource_id = "underworld-gate"
+    ),
+    area.Area(
+        name = "Church",
+        desc = "You may draw a White Card.",
+        domain = [6],
+        action = lambda gc, player: player.drawCard(gc.white_cards),
+        resource_id = "church"
+    ),
+    area.Area(
+        name = "Cemetery",
+        desc = "You may draw a Black Card.",
+        domain = [8],
+        action = lambda gc, player: player.drawCard(gc.black_cards),
+        resource_id = "cemetery"
+    ),
+    area.Area(
+        name = "Weird Woods",
+        desc = "You may either give 2 damage to any player or heal 1 damage of any player.",
+        domain = [9],
+        action = lambda gc, player: 0,  # TODO
+        resource_id = "weird-woods"
+    ),
+    area.Area(
+        name = "Erstwhile Altar",
+        desc = "You may steal an equipment card from any player.",
+        domain = [10],
+        action = lambda gc, player: 0,  # TODO
+        resource_id = "erstwhile-altar"
+    )
+]
