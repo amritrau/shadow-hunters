426a3914aa860e90f22eb5bfbaf8253ba5661909
amritrau <amritrau@gmail.com>
2019-02-26 17:21:04 -0500

    Hotfix 6

--- shadow-hunters/player.py
+++ shadow-hunters/player.py
@@ -11,6 +11,7 @@ class Player:
 
     def setCharacter(self, character):
         self.character = character
+        self.win_cond = win_cond
         self.hp = self.character.max_hp
 
     def reveal(self):
