71ab7a7e06c60acaec205304b1c96137c89b1ee8
Max Levatich <maxwell.levatich@yale.edu>
2019-03-02 19:56:55 +0000

    fixed test area

--- shadow-hunters/tests/test_area.py
+++ shadow-hunters/tests/test_area.py
@@ -9,7 +9,7 @@ def test_init():
         name = "Weird Woods",
         desc = "You may either give 2 damage to any player or heal 1 damage of any player.",
         domain = [9],
-        action = weird_woods_action,
+        action = lambda: 0,
         resource_id = "weird-woods"
     )
     assert 1
