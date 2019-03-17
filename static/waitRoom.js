964b86ab43e580feab80f392179e6d02528b16f5
giareyes <giareyes2005@gmail.com>
2019-03-17 02:07:33 -0700

    split classes into different files. erased unecessary code.

---
+++ static/waitRoom.js
@@ -0,0 +1,32 @@
+// this is the scene that will be used in the waiting room, i.e. before the game starts
+var WaitingRoom = new Phaser.Class ({
+    Extends: Phaser.Scene,
+
+    initialize:
+
+    function Room () {
+        Phaser.Scene.call(this, {key: 'menu'});
+    },
+    preload: function () {
+        // url where gfx resources are stored
+        var gfx = "https://s3.amazonaws.com/shadowhunters.gfxresources/";
+        this.load.image('background', gfx + 'background-1066.png');
+    },
+
+    create: function () {
+        var self = this;
+
+        //this resizes the background image to fit into our canvas
+        var background = this.add.image(533, 300, 'background');
+        background.displayWidth = this.sys.canvas.width;
+        background.displayHeight = this.sys.canvas.height;
+
+        //SOCKET CALL! if game start button pressed, then we switch scenes and go to the game board
+        socket.on("game_start", function (data) {
+            $('#start').remove();
+            var html = '<p id="wait">Waiting...</p>';
+            $('#game').append(html);
+            self.scene.start('board', data);
+        });
+    }
+});
