c883c17479343d2866f65cba305d9c73b2fc9757
mlevatich <maxwell.levatich@yale.edu>
2019-04-18 01:38:00 -0400

    Black card tests working

--- shadow-hunters/tests/element_tests/test_black_cards.py
+++ shadow-hunters/tests/element_tests/test_black_cards.py
@@ -37,7 +37,25 @@ def test_vampire_bat():
     assert len([d for d in damages if d == 0]) == len(gc.players) - 1
 
 def test_moody_goblin():
-    assert 1
+
+    # Setup rigged game context
+    gc, ef = helpers.fresh_gc_ef()
+    p1 = gc.players[0]
+    p2 = gc.players[1]
+    c = helpers.get_card_by_title(ef, "Moody Goblin")
+
+    # Check that nothing happens if no one has equipment
+    c.use({ 'self': p2, 'card': c })
+    assert not p2.equipment
+
+    # Give p1 holy robe
+    roly_hobe = helpers.get_card_by_title(ef, "Holy Robe")
+    p1.equipment.append(roly_hobe)
+
+    # Check that robe is stolen
+    c.use({ 'self': p2, 'card': c })
+    assert p2.equipment == [roly_hobe]
+    assert not p1.equipment
 
 def test_diabolic_ritual():
 
@@ -69,7 +87,67 @@ def test_diabolic_ritual():
     assert s.state == 1 and s.damage == 0
 
 def test_banana_peel():
-    assert 1
+
+    # Setup rigged game context
+    gc, ef = helpers.fresh_gc_ef()
+    p1 = gc.players[0]
+    p2 = gc.players[1]
+    gc.ask_h = helpers.answer_sequence([
+        "Receive 1 damage",
+        "Give an equipment card",
+        "Holy Robe",
+        p2.user_id
+    ])
+    c = helpers.get_card_by_title(ef, "Banana Peel")
+
+    # Give p1 holy robe
+    roly_hobe = helpers.get_card_by_title(ef, "Holy Robe")
+    p1.equipment.append(roly_hobe)
+
+    # Check take one damage
+    assert p1.damage == 0
+    c.use({ 'self': p1, 'card': c })
+    assert p1.damage == 1
+
+    # Check give away equipment
+    c.use({ 'self': p1, 'card': c })
+    assert p1.damage == 1
+    assert not p1.equipment
+    assert p2.equipment == [roly_hobe]
+
+def test_dynamite():
+
+    # Setup rigged game context
+    gc, ef = helpers.fresh_gc_ef(6)
+    p1 = gc.players[0]
+    c = helpers.get_card_by_title(ef, "Dynamite")
+
+    # Put one player in each area
+    area_names = ["Church", "Cemetery", "Erstwhile Altar", "Weird Woods", "Hermit\'s Cabin", "Underworld Gate"]
+    areas = [helpers.get_area_by_name(gc, a) for a in area_names]
+    for p in gc.players:
+        p.move(areas.pop(0))
+
+    # Check that only one person took 3 damage
+    c.use({ 'self': p1, 'card': c })
+    damages = [p.damage for p in gc.players]
+    assert len([d for d in damages if d == 3]) == 1
+    assert len([d for d in damages if d == 0]) == len(gc.players) - 1
+
 
 def test_spiritual_doll():
-    assert 1
+
+    # Setup rigged game context
+    gc, ef = helpers.fresh_gc_ef()
+    p1 = gc.players[0]
+    p2 = gc.players[1]
+    c = helpers.get_card_by_title(ef, "Spiritual Doll")
+    gc.ask_h = helpers.answer_sequence([
+        "Use Spiritual Doll",
+        p2.user_id,
+        "Roll the 6-sided die!"
+    ])
+
+    # Check that someone gets hit
+    c.use({ 'self': p1, 'card': c })
+    assert bool(p1.damage == 3) != bool(p2.damage == 3)
