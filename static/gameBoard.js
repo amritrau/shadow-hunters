e73642bb35e8f538b742d492a379c4095b395033
Gia Reyes <giareyes2005@gmail.com>
2019-04-19 19:35:03 -0400

    added special ability button

--- static/gameBoard.js
+++ static/gameBoard.js
@@ -587,6 +587,7 @@ var GameBoard = new Phaser.Class ({
             // remove reveal button on person's screen if they are revealed
             if((data.state == 1 || data.state == 0) && $('#reveal').length) {
                 $('#reveal').remove();
+                $('#special').show();
             }
         }
 
