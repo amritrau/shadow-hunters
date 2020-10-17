f0f3a24faca1ac4d11a732c3c45843039e2abe90
amritrau <amritrau@gmail.com>
2020-10-17 08:48:40 -0700

    Improve documentation

--- shadow-hunters/agent.py
+++ shadow-hunters/agent.py
@@ -9,10 +9,12 @@ class AgentInterface():
 
     def choose_action(self, options, player, gc):
         """Choose an action from the options provided"""
+
         raise NotImplementedError
 
     def choose_reveal(self, player, gc):
         """Return true if the agent chooses to reveal"""
+        
         raise NotImplementedError
 
 
@@ -20,14 +22,16 @@ class RandomAgent(AgentInterface):
     """Defines an agent that randomly interacts with the game"""
 
     def choose_action(self, options, player, gc):
-        """Choose an action from the options provided"""
+        """Randomly select an action from the options provided"""
 
         if 'Decline' in options and len(options) > 1:
             options.remove('Decline')
         return {'value': random.choice(options)}
 
     def choose_reveal(self, player, gc):
-        """Return true if the agent chooses to reveal"""
+        """Randomly reveal identity with increasing probability as the
+        game progresses"""
+
         reveal_chance = gc.round_count / 20
         return (random.random() <= reveal_chance)
 
