62af28a81b81b6ccc2cfa414e34a4df7a6babdb8
mlevatich <ml4553@columbia.edu>
2021-02-24 21:16:39 -0500

    more specials cleanup

--- shadow-hunters/specials.py
+++ shadow-hunters/specials.py
@@ -5,19 +5,18 @@
 
 def allie(gc, player, turn_pos):
     # ANY TIME
-    if turn_pos == 'now':
-        if not player.modifiers['special_used']:
+    if turn_pos == 'now' and not player.modifiers['special_used']:
 
-            # Tell
-            gc.tell_h("{} ({}) used their special ability: {}", [
-                      player.user_id, player.character.name,
-                      player.character.special_desc])
+        # Tell
+        gc.tell_h("{} ({}) used their special ability: {}", [
+                  player.user_id, player.character.name,
+                  player.character.special_desc])
 
-            # Full heal
-            player.setDamage(0, player)
+        # Full heal
+        player.setDamage(0, player)
 
-            # Update modifiers
-            player.modifiers['special_used'] = True
+        # Update modifiers
+        player.modifiers['special_used'] = True
 
 
 def bob(gc, player, turn_pos):
@@ -25,13 +24,12 @@ def bob(gc, player, turn_pos):
         if 4 <= len(gc.players) <= 6:
             player.modifiers['steal_for_damage'] = True
         else:
-            # Update modifiers
             player.modifiers['steal_all_on_kill'] = True
 
 
 def catherine(gc, player, turn_pos):
     # START OF TURN
-    if turn_pos == 'start' and (not player.modifiers['special_used']):
+    if turn_pos == 'start' and not player.modifiers['special_used']:
 
         # Tell
         gc.tell_h("{} ({}) used their special ability: {}", [
@@ -50,19 +48,18 @@ def charles(gc, player, turn_pos):
 
 def gregor(gc, player, turn_pos):
     # END OF TURN
-    if turn_pos == 'end':
-        if not player.modifiers['special_used']:
-            player.modifiers['special_used'] = True
+    if turn_pos == 'end' and not player.modifiers['special_used']:
+        player.modifiers['special_used'] = True
 
-            # Tell
-            gc.tell_h("{} ({}) used their special ability: {}", [
-                      player.user_id, player.character.name,
-                      player.character.special_desc])
+        # Tell
+        gc.tell_h("{} ({}) used their special ability: {}", [
+                  player.user_id, player.character.name,
+                  player.character.special_desc])
 
-            player.modifiers['barrier'] = True
+        player.modifiers['barrier'] = True
 
-            gc.tell_h("{} activated a Ghostly Barrier and is immune to damage!",
-                      [player.user_id])
+        gc.tell_h("{} activated a Ghostly Barrier and is immune to damage!",
+                  [player.user_id])
 
     # START OF NEXT TURN
     if turn_pos == 'start' and player.modifiers['barrier']:
@@ -71,94 +68,90 @@ def gregor(gc, player, turn_pos):
 
 def george(gc, player, turn_pos):
     # START OF TURN
-    if turn_pos == 'start':
-        if not player.modifiers['special_used']:
-            player.modifiers['special_used'] = True
+    if turn_pos == 'start' and not player.modifiers['special_used']:
+        player.modifiers['special_used'] = True
 
-            # Tell
-            gc.tell_h("{} ({}) used their special ability: {}", [
-                      player.user_id, player.character.name,
-                      player.character.special_desc])
+        # Tell
+        gc.tell_h("{} ({}) used their special ability: {}", [
+                  player.user_id, player.character.name,
+                  player.character.special_desc])
 
-            # Present player with list of attack options
-            target_Player = player.choosePlayer()
+        # Present player with list of attack options
+        target_Player = player.choosePlayer()
 
-            # Roll and give damage to target
-            roll_result = player.rollDice('4')
-            old = target_Player.damage
-            new = target_Player.moveDamage(-1 * roll_result, player)
-            gc.tell_h("{}'s Hammer gave {} {} damage!", [
-                      player.user_id, target_Player.user_id, new - old])
+        # Roll and give damage to target
+        roll_result = player.rollDice('4')
+        old = target_Player.damage
+        new = target_Player.moveDamage(-1 * roll_result, player)
+        gc.tell_h("{}'s Hammer gave {} {} damage!", [
+                  player.user_id, target_Player.user_id, new - old])
 
 
 def fuka(gc, player, turn_pos):
     # START OF TURN
-    if turn_pos == 'start':
-        if not player.modifiers['special_used']:
-            player.modifiers['special_used'] = True
+    if turn_pos == 'start' and not player.modifiers['special_used']:
+        player.modifiers['special_used'] = True
 
-            # Tell
-            gc.tell_h("{} ({}) used their special ability: {}", [
-                      player.user_id, player.character.name,
-                      player.character.special_desc])
+        # Tell
+        gc.tell_h("{} ({}) used their special ability: {}", [
+                  player.user_id, player.character.name,
+                  player.character.special_desc])
 
-            # Enter set damage to 7 sequence
-            # Select a player to use special on (includes user)
-            player.gc.ask_h(
-                'confirm', {'options': ["Use special ability"]},
-                player.user_id)
+        # Enter set damage to 7 sequence
+        # Select a player to use special on (includes user)
+        player.gc.ask_h(
+            'confirm', {'options': ["Use special ability"]},
+            player.user_id)
 
-            # Set selected player to 7 damage
-            target_Player = player.choosePlayer(include_self=True)
-            target_Player.setDamage(7, player)
-            gc.tell_h("{} gave a killing cure to {}!", [
-                      player.user_id, target_Player.user_id])
+        # Set selected player to 7 damage
+        target_Player = player.choosePlayer(include_self=True)
+        target_Player.setDamage(7, player)
+        gc.tell_h("{} gave a killing cure to {}!", [
+                  player.user_id, target_Player.user_id])
 
 
 def franklin(gc, player, turn_pos):
+    # START OF TURN
+    if turn_pos == 'start' and not player.modifiers['special_used']:
+        player.modifiers['special_used'] = True
 
-    if turn_pos == 'start':
-        if not player.modifiers['special_used']:
-            player.modifiers['special_used'] = True
+        # Tell
+        gc.tell_h("{} ({}) used their special ability: {}", [
+                  player.user_id, player.character.name,
+                  player.character.special_desc])
 
-            # Tell
-            gc.tell_h("{} ({}) used their special ability: {}", [
-                      player.user_id, player.character.name,
-                      player.character.special_desc])
+        # Present player with list of attack options
+        target_Player = player.choosePlayer()
 
-            # Present player with list of attack options
-            target_Player = player.choosePlayer()
-
-            # Roll and give damage to target
-            roll_result = player.rollDice('6')
-            old = target_Player.damage
-            new = target_Player.moveDamage(-1 * roll_result, player)
-            gc.tell_h("{}'s Lightning gave {} {} damage!", [
-                      player.user_id, target_Player.user_id, new - old])
+        # Roll and give damage to target
+        roll_result = player.rollDice('6')
+        old = target_Player.damage
+        new = target_Player.moveDamage(-1 * roll_result, player)
+        gc.tell_h("{}'s Lightning gave {} {} damage!", [
+                  player.user_id, target_Player.user_id, new - old])
 
 
 def ellen(gc, player, turn_pos):
     # START OF TURN
-    if turn_pos == 'start':
-        if not player.modifiers['special_used']:
-            player.modifiers['special_used'] = True
+    if turn_pos == 'start' and not player.modifiers['special_used']:
+        player.modifiers['special_used'] = True
 
-            # Tell
-            gc.tell_h("{} ({}) used their special ability: {}", [
-                      player.user_id, player.character.name,
-                      player.character.special_desc])
+        # Tell
+        gc.tell_h("{} ({}) used their special ability: {}", [
+                  player.user_id, player.character.name,
+                  player.character.special_desc])
 
-            # Choose a player to cancel their special
-            target_Player = player.choosePlayer()
+        # Choose a player to cancel their special
+        target_Player = player.choosePlayer()
 
-            # Cancel special
-            target_Player.resetModifiers()
-            target_Player.modifiers['special_used'] = True
-            target_Player.special = lambda gc, player, turn_pos: gc.tell_h(
-                "Your special ability was voided by {}.", [player.user_id],
-                player.socket_id)
-            msg = "{} voided {}'s special ability for the rest of the game!"
-            gc.tell_h(msg, [player.user_id, target_Player.user_id])
+        # Cancel special
+        target_Player.resetModifiers()
+        target_Player.modifiers['special_used'] = True
+        target_Player.special = lambda gc, player, turn_pos: gc.tell_h(
+            "Your special ability was voided by {}.", [player.user_id],
+            player.socket_id)
+        msg = "{} voided {}'s special ability for the rest of the game!"
+        gc.tell_h(msg, [player.user_id, target_Player.user_id])
 
 # Shadows
 
