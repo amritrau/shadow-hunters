7fd62b41f878f13f2dc0ede18c372a9aec51ea58
mlevatich <ml4553@columbia.edu>
2021-02-27 17:23:58 -0500

    forgot '=' in hash

--- shadow-hunters/tests/integration_tests/test_gameplay.py
+++ shadow-hunters/tests/integration_tests/test_gameplay.py
@@ -35,6 +35,6 @@ def test_regression():
             gc, ef = fresh_gc_ef(n)
             game_hashes += gc.play(debug=True)
 
-    correct_hash = 'kimaF/F7Ozy3AdpVxnVUk0GwDuX0t1qdGrN69M+91zI'
+    correct_hash = 'kimaF/F7Ozy3AdpVxnVUk0GwDuX0t1qdGrN69M+91zI='
     found_hash = make_hash_sha256(game_hashes)
     assert correct_hash == found_hash
